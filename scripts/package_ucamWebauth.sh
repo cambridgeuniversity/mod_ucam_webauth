dee9cac959bda6fa7342e82a9c7b857ac27b80a0
Sam Wenham <sam@wenhams.co.uk>
2018-02-21 19:09:45 +0000

    Also build for i386

--- scripts/package_ucamWebauth.sh
+++ scripts/package_ucamWebauth.sh
@@ -2,8 +2,16 @@
 
 sbuild -vd xenial libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-16.04'
 
+sbuild -vd xenial libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-16.04' --arch=i386
+
 sbuild -vd trusty libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-14.04'
+
+sbuild -vd trusty libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-14.04' --arch=i386
 
 sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8'
 
+sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8' --arch=i386
+
 sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9'
+
+sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9' --arch=i386
