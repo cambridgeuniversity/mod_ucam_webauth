bf254fcd311625f7e0c6afa62beed20f36209748
Sam Wenham <sam@wenhams.co.uk>
2018-02-21 18:11:46 +0000

    Build debs for xenial, trusty, stretch and jessie

--- scripts/package_ucamWebauth.sh
+++ scripts/package_ucamWebauth.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sbuild -vd xenial libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu'
+sbuild -vd xenial libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-16.04'
+
+sbuild -vd trusty libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-14.04'
+
+sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8'
+
+sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9'
