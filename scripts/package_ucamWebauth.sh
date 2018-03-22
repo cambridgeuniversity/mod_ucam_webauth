90d3bdc935951d316b688eae9c4ca760156146e3
Sam Wenham <sam@wenhams.co.uk>
2018-03-22 18:33:55 +0000

    Build the correct env for the name!

--- scripts/package_ucamWebauth.sh
+++ scripts/package_ucamWebauth.sh
@@ -8,10 +8,10 @@ sbuild -vd trusty libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package 
 
 sbuild -vd trusty libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~ubuntu-14.04' --arch=i386
 
-sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8'
+sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8'
 
-sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8' --arch=i386
+sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-8' --arch=i386
 
-sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9'
+sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9'
 
-sbuild -vd jessie libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9' --arch=i386
+sbuild -vd stretch libapache2-mod-ucam-webauth_2.0.5apache24.dsc -m "UIS Package Builder" --append-to-version='~debian-9' --arch=i386
